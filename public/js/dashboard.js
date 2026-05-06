6e941cc173ac4c40efa1a2ee0a50bcfb7ebb0306
M-jahnavi08 <mommineedijahnavisatya@gmail.com>
2026-05-06 14:43:26 +0530

    Delete public directory

--- public/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-fetch("/api/stats")
-.then(res => res.json())
-.then(data => {
-
-document.getElementById("totalProducts").innerText = data.totalProducts || 0;
-document.getElementById("totalStock").innerText = data.totalStock || 0;
-document.getElementById("lowStock").innerText = data.lowStock || 0;
-
-});
